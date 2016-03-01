4f3a6448b063095453b8de22e503de48f710dcba
Marcus <marcusbullock@hotmail.co.uk>
2016-03-01 10:10:09 +0000

    First routes defined & working

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,28 @@ feature 'restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Hawksmoor')
+    end
+
+    scenario 'should correctly display the added restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'Hawksmoor'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
+
+  context 'user adding restaurants' do
+    scenario 'should display the restaurant the user added' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Hawksmoor'
+      click_button 'Create Restaurant'
+      expect(page).to have_content('Hawksmoor')
+      expect(current_path).to eq('/restaurants')
+    end
+  end
+
 end
