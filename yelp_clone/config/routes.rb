6943dd5b8590416f9585e6492dc41e2a986849ab
Marcus <marcusbullock@hotmail.co.uk>
2016-03-02 10:08:05 +0000

    Facebook AUTH working

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'restaurants' => 'restaurants#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  root to: "restaurants#index"
 
-  resources :restaurants do 
+  resources :restaurants do
     resources :reviews
   end
   # The priority is based upon order of creation: first created -> highest priority.
